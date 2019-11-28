197febfed557aaec3d1e89d44469467686be8180
Mark Kenneth Ranosa <mark@pearlpay.io>
2019-11-28 08:35:31 +0800

    Moved localstack environment variable reference from function to global level

--- src/model/note.py
+++ src/model/note.py
@@ -7,8 +7,8 @@ from pynamodb.attributes import UnicodeAttribute
 class Note(Model):
     class Meta:
         table_name = 'Note'
-        host = ('http://' + os.environ.get('LOCALSTACK-HOSTNAME',
-                                           'None') + ':4569') if 'LOCALSTACK-HOSTNAME' in os.environ else None
+        host = ('http://' + os.environ.get('LOCALSTACK-HOSTNAME') + ':4569') if os.environ.get(
+            'LOCALSTACK-HOSTNAME') != 'none' else None
 
     note_id = UnicodeAttribute(hash_key=True)
     title = UnicodeAttribute(null=True)
